c605ee036ff813e41e4d9555f681a3e50ae0474f
hema <hema.1998.man@gmail.com>
2022-07-16 02:24:33 +0200

    asasedqw

--- js/selector.js
+++ js/selector.js
@@ -19,17 +19,17 @@ const new_ele = (parent, ele, txt, classes, attribute) => {
 
 const start = () => {
     clearall();
-
+    // question part 
     const anc = new_ele(container, 'a', null,null, { name: 'href', val: '#' })
     const div = new_ele(anc, 'div', null, 'card mb-3', null);
     const div2 = new_ele(div, 'div', null, 'card-body', null);
     new_ele(div2, 'h5', data[count].question, 'card-title', null);
 
     arr = Object.keys(data[count].answers);
-
+    // answer part 
     for (let i = 0; i < arr.length; i++) {
         const anc2 = new_ele(container, 'a', null,null, { name: 'href', val: '#' })
-        const div3 = new_ele(anc2, 'div', null, 'card mb-3', { name: 'onclick', val: `next()` });
+        const div3 = new_ele(anc2, 'div', null, 'card mb-3 ans', { name: 'onclick', val: `next()` });
         const div4 = new_ele(div3, 'div', null, 'card-body', null);
         new_ele(div4, 'h5', data[count].answers[arr[i]], 'card-title', null);
     }
